b570ec7f66edab9fe1a1bd20ed05c82ab8314718
Anastasiya Petrichuk <apetrichuk@ozon.ru>
2023-04-22 21:13:13 +0300

    прописаны сериалайзеры

---
+++ api/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+
+from internal.models import User, Research, Graph, Note, NodesNotesRelation
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'last_name', 'first_name', 'surname', 'study_group', 'username', 'email', 'is_staff',
+                  'is_active', 'last_login')
+
+
+class ResearchSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Research
+        fields = ('rsrch_id', 'title', 'description', 'start_date', 'end_date',
+                  'get_researchers_ids')
+
+
+class GraphSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Graph
+        fields = ('graph_id', 'data', 'title', 'research_id')
+
+
+class NoteSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Note
+        fields = ('note_id', 'url', 'note_type', 'user_id')
+
+
+class NodesNotesRelationSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = NodesNotesRelation
+        fields = ('id', 'node_id', 'note_id', 'graph_id')
