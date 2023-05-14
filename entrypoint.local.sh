3179d61948ec9f03c1524ca60fecec1fa5a90a82
Anastasiya Petrichuk <apetrichuk@ozon.ru>
2023-05-14 18:39:44 +0300

    настроен локальный запуск через скрипт

---
+++ entrypoint.local.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# установка всех переменных среды из файла ./config/.env.local
+# без учета строк начинающихся на "#"
+unamestr=$(uname)
+echo $unamestr
+if [ "$unamestr" = 'Linux' ]; then
+  export $(grep -v '^#' ./config/.env.local | xargs -d '\n')
+elif [ "$unamestr" = 'FreeBSD' ] || [ "$unamestr" = 'Darwin' ]; then
+  export $(grep -v '^#' ./config/.env.local | xargs -0)
+fi
+
+# завершение скрипта, если при установке переменных среды были ошибки
+if [ $? -eq 0 ]; then
+  echo "Successfully export env variables"
+else
+  echo "Could not export env variables" >&2
+  exit
+fi
+
+# эту строку можно комментить и запускать самостоятельно
+python manage.py flush --no-input # сброс таблиц
+
+python manage.py makemigrations
+python manage.py migrate
+
+# эту строку можно комментить и запускать самостоятельно
+python manage.py createcachetable
+
+python manage.py createsuperuser \
+  --noinput \
+  --username $DJANGO_SUPERUSER_USERNAME \
+  --email $DJANGO_SUPERUSER_EMAIL
+
+python manage.py loaddata dev/subjects.json
+python manage.py runserver
+
+exec "$@"
